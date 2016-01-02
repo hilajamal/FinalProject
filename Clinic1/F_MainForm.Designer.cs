465f5eee36b5f346fcfef8bd07d6aa7a7c0f15b4
hilajamal <hilajamal@gmail.com>
2016-01-02 20:21:32 +0200

    kku

--- Clinic1/F_MainForm.Designer.cs
+++ Clinic1/F_MainForm.Designer.cs
@@ -37,6 +37,7 @@
             this.btnReports = new System.Windows.Forms.Button();
             this.btnIntake = new System.Windows.Forms.Button();
             this.BtnIntakeBoard = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -155,12 +156,25 @@
             this.BtnIntakeBoard.UseVisualStyleBackColor = false;
             this.BtnIntakeBoard.Click += new System.EventHandler(this.BtnIntakeBoard_Click);
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Purple;
+            this.button2.Font = new System.Drawing.Font("David", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(359, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 136);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "אינטייק";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
             // F_MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(1086, 477);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.BtnIntakeBoard);
             this.Controls.Add(this.btnIntake);
             this.Controls.Add(this.btnReports);
@@ -188,6 +202,7 @@
         private System.Windows.Forms.Button btnReports;
         private System.Windows.Forms.Button btnIntake;
         private System.Windows.Forms.Button BtnIntakeBoard;
+        private System.Windows.Forms.Button button2;
 
     }
 }
