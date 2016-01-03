fe3456a7d246322c8af6c6d30103944abe4d701f
hilajamal <hilajamal@gmail.com>
2016-01-03 22:55:31 +0200

    ככ

--- Clinic1/F_Groups.Designer.cs
+++ Clinic1/F_Groups.Designer.cs
@@ -75,30 +75,30 @@
             // LblName
             // 
             this.LblName.AutoSize = true;
-            this.LblName.Font = new System.Drawing.Font("David", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblName.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblName.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.LblName.Location = new System.Drawing.Point(715, 71);
+            this.LblName.Location = new System.Drawing.Point(485, 49);
             this.LblName.Name = "LblName";
-            this.LblName.Size = new System.Drawing.Size(100, 20);
+            this.LblName.Size = new System.Drawing.Size(77, 16);
             this.LblName.TabIndex = 33;
-            this.LblName.Text = ":שם קבוצה";
+            this.LblName.Text = "שם קבוצה:";
             // 
             // LblID
             // 
             this.LblID.AutoSize = true;
-            this.LblID.Font = new System.Drawing.Font("David", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblID.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblID.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.LblID.Location = new System.Drawing.Point(699, 30);
+            this.LblID.Location = new System.Drawing.Point(484, 21);
             this.LblID.Name = "LblID";
-            this.LblID.Size = new System.Drawing.Size(117, 20);
+            this.LblID.Size = new System.Drawing.Size(91, 16);
             this.LblID.TabIndex = 32;
-            this.LblID.Text = ":מספר קבוצה";
+            this.LblID.Text = "מספר קבוצה:";
             // 
             // txtGroupNumberAdd
             // 
             this.txtGroupNumberAdd.Enabled = false;
             this.txtGroupNumberAdd.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtGroupNumberAdd.Location = new System.Drawing.Point(593, 27);
+            this.txtGroupNumberAdd.Location = new System.Drawing.Point(378, 18);
             this.txtGroupNumberAdd.Name = "txtGroupNumberAdd";
             this.txtGroupNumberAdd.Size = new System.Drawing.Size(100, 23);
             this.txtGroupNumberAdd.TabIndex = 34;
@@ -107,7 +107,7 @@
             // txtGroupNameAdd
             // 
             this.txtGroupNameAdd.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtGroupNameAdd.Location = new System.Drawing.Point(471, 69);
+            this.txtGroupNameAdd.Location = new System.Drawing.Point(257, 49);
             this.txtGroupNameAdd.Name = "txtGroupNameAdd";
             this.txtGroupNameAdd.Size = new System.Drawing.Size(222, 23);
             this.txtGroupNameAdd.TabIndex = 35;
@@ -116,7 +116,7 @@
             // 
             this.CmbPatientNameAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CmbPatientNameAdd.FormattingEnabled = true;
-            this.CmbPatientNameAdd.Location = new System.Drawing.Point(573, 155);
+            this.CmbPatientNameAdd.Location = new System.Drawing.Point(143, 121);
             this.CmbPatientNameAdd.Name = "CmbPatientNameAdd";
             this.CmbPatientNameAdd.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.CmbPatientNameAdd.Size = new System.Drawing.Size(166, 24);
@@ -126,7 +126,7 @@
             // 
             this.CmblPatientIdAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CmblPatientIdAdd.FormattingEnabled = true;
-            this.CmblPatientIdAdd.Location = new System.Drawing.Point(573, 124);
+            this.CmblPatientIdAdd.Location = new System.Drawing.Point(143, 90);
             this.CmblPatientIdAdd.Name = "CmblPatientIdAdd";
             this.CmblPatientIdAdd.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.CmblPatientIdAdd.Size = new System.Drawing.Size(166, 24);
@@ -137,7 +137,7 @@
             this.LblPatientName.AutoSize = true;
             this.LblPatientName.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblPatientName.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.LblPatientName.Location = new System.Drawing.Point(745, 157);
+            this.LblPatientName.Location = new System.Drawing.Point(315, 123);
             this.LblPatientName.Name = "LblPatientName";
             this.LblPatientName.Size = new System.Drawing.Size(71, 16);
             this.LblPatientName.TabIndex = 38;
@@ -148,7 +148,7 @@
             this.LblPatientId.AutoSize = true;
             this.LblPatientId.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblPatientId.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.LblPatientId.Location = new System.Drawing.Point(744, 128);
+            this.LblPatientId.Location = new System.Drawing.Point(314, 94);
             this.LblPatientId.Name = "LblPatientId";
             this.LblPatientId.Size = new System.Drawing.Size(72, 16);
             this.LblPatientId.TabIndex = 37;
@@ -159,7 +159,7 @@
             this.BtnAddToGroupAdd.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.BtnAddToGroupAdd.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnAddToGroupAdd.ForeColor = System.Drawing.Color.Black;
-            this.BtnAddToGroupAdd.Location = new System.Drawing.Point(471, 133);
+            this.BtnAddToGroupAdd.Location = new System.Drawing.Point(41, 99);
             this.BtnAddToGroupAdd.Name = "BtnAddToGroupAdd";
             this.BtnAddToGroupAdd.Size = new System.Drawing.Size(83, 40);
             this.BtnAddToGroupAdd.TabIndex = 41;
@@ -195,7 +195,7 @@
             dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.DgPatientsAdd.DefaultCellStyle = dataGridViewCellStyle3;
             this.DgPatientsAdd.Enabled = false;
-            this.DgPatientsAdd.Location = new System.Drawing.Point(58, 206);
+            this.DgPatientsAdd.Location = new System.Drawing.Point(58, 159);
             this.DgPatientsAdd.Name = "DgPatientsAdd";
             this.DgPatientsAdd.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
@@ -209,7 +209,7 @@
             dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle5.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.DgPatientsAdd.RowsDefaultCellStyle = dataGridViewCellStyle5;
-            this.DgPatientsAdd.Size = new System.Drawing.Size(736, 216);
+            this.DgPatientsAdd.Size = new System.Drawing.Size(566, 216);
             this.DgPatientsAdd.TabIndex = 42;
             // 
             // TabGroups
@@ -227,7 +227,7 @@
             this.TabGroups.RightToLeftLayout = true;
             this.TabGroups.SelectedIndex = 0;
             this.TabGroups.ShowToolTips = true;
-            this.TabGroups.Size = new System.Drawing.Size(950, 584);
+            this.TabGroups.Size = new System.Drawing.Size(685, 475);
             this.TabGroups.TabIndex = 44;
             this.TabGroups.SelectedIndexChanged += new System.EventHandler(this.TabGroups_SelectedIndexChanged);
             // 
@@ -248,7 +248,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 32);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(942, 548);
+            this.tabPage1.Size = new System.Drawing.Size(677, 439);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "הוספת קבוצה";
             // 
@@ -257,7 +257,7 @@
             this.btnSaveNewGroupUpdate.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.btnSaveNewGroupUpdate.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSaveNewGroupUpdate.ForeColor = System.Drawing.Color.Black;
-            this.btnSaveNewGroupUpdate.Location = new System.Drawing.Point(390, 438);
+            this.btnSaveNewGroupUpdate.Location = new System.Drawing.Point(343, 392);
             this.btnSaveNewGroupUpdate.Name = "btnSaveNewGroupUpdate";
             this.btnSaveNewGroupUpdate.Size = new System.Drawing.Size(83, 40);
             this.btnSaveNewGroupUpdate.TabIndex = 43;
@@ -282,7 +282,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 32);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(942, 548);
+            this.tabPage2.Size = new System.Drawing.Size(677, 439);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "עדכון קבוצה";
             // 
@@ -291,7 +291,7 @@
             this.btnRemoveFromGroup.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.btnRemoveFromGroup.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRemoveFromGroup.ForeColor = System.Drawing.Color.Black;
-            this.btnRemoveFromGroup.Location = new System.Drawing.Point(13, 242);
+            this.btnRemoveFromGroup.Location = new System.Drawing.Point(18, 250);
             this.btnRemoveFromGroup.Name = "btnRemoveFromGroup";
             this.btnRemoveFromGroup.Size = new System.Drawing.Size(83, 40);
             this.btnRemoveFromGroup.TabIndex = 75;
@@ -304,7 +304,7 @@
             this.btnAddToGroupUpdate.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.btnAddToGroupUpdate.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAddToGroupUpdate.ForeColor = System.Drawing.Color.Black;
-            this.btnAddToGroupUpdate.Location = new System.Drawing.Point(258, 35);
+            this.btnAddToGroupUpdate.Location = new System.Drawing.Point(96, 59);
             this.btnAddToGroupUpdate.Name = "btnAddToGroupUpdate";
             this.btnAddToGroupUpdate.Size = new System.Drawing.Size(83, 40);
             this.btnAddToGroupUpdate.TabIndex = 74;
@@ -316,7 +316,7 @@
             // 
             this.cmbPatientNameUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbPatientNameUpdate.FormattingEnabled = true;
-            this.cmbPatientNameUpdate.Location = new System.Drawing.Point(358, 57);
+            this.cmbPatientNameUpdate.Location = new System.Drawing.Point(196, 81);
             this.cmbPatientNameUpdate.Name = "cmbPatientNameUpdate";
             this.cmbPatientNameUpdate.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.cmbPatientNameUpdate.Size = new System.Drawing.Size(166, 24);
@@ -326,7 +326,7 @@
             // 
             this.cmbPatientIDUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbPatientIDUpdate.FormattingEnabled = true;
-            this.cmbPatientIDUpdate.Location = new System.Drawing.Point(358, 26);
+            this.cmbPatientIDUpdate.Location = new System.Drawing.Point(196, 50);
             this.cmbPatientIDUpdate.Name = "cmbPatientIDUpdate";
             this.cmbPatientIDUpdate.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.cmbPatientIDUpdate.Size = new System.Drawing.Size(166, 24);
@@ -337,7 +337,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(530, 59);
+            this.label1.Location = new System.Drawing.Point(368, 83);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 16);
             this.label1.TabIndex = 71;
@@ -348,7 +348,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(529, 30);
+            this.label2.Location = new System.Drawing.Point(367, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 16);
             this.label2.TabIndex = 70;
@@ -358,7 +358,7 @@
             // 
             this.CmbGroupNameUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CmbGroupNameUpdate.FormattingEnabled = true;
-            this.CmbGroupNameUpdate.Location = new System.Drawing.Point(623, 57);
+            this.CmbGroupNameUpdate.Location = new System.Drawing.Point(461, 81);
             this.CmbGroupNameUpdate.Name = "CmbGroupNameUpdate";
             this.CmbGroupNameUpdate.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.CmbGroupNameUpdate.Size = new System.Drawing.Size(124, 24);
@@ -368,7 +368,7 @@
             // 
             this.CmbGroupIdUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CmbGroupIdUpdate.FormattingEnabled = true;
-            this.CmbGroupIdUpdate.Location = new System.Drawing.Point(623, 26);
+            this.CmbGroupIdUpdate.Location = new System.Drawing.Point(461, 50);
             this.CmbGroupIdUpdate.Name = "CmbGroupIdUpdate";
             this.CmbGroupIdUpdate.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.CmbGroupIdUpdate.Size = new System.Drawing.Size(124, 24);
@@ -380,7 +380,7 @@
             this.LblGrouptName.AutoSize = true;
             this.LblGrouptName.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblGrouptName.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.LblGrouptName.Location = new System.Drawing.Point(753, 59);
+            this.LblGrouptName.Location = new System.Drawing.Point(591, 83);
             this.LblGrouptName.Name = "LblGrouptName";
             this.LblGrouptName.Size = new System.Drawing.Size(71, 16);
             this.LblGrouptName.TabIndex = 67;
@@ -391,7 +391,7 @@
             this.LblGroupNumber.AutoSize = true;
             this.LblGroupNumber.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LblGroupNumber.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.LblGroupNumber.Location = new System.Drawing.Point(752, 30);
+            this.LblGroupNumber.Location = new System.Drawing.Point(590, 54);
             this.LblGroupNumber.Name = "LblGroupNumber";
             this.LblGroupNumber.Size = new System.Drawing.Size(85, 16);
             this.LblGroupNumber.TabIndex = 66;
@@ -424,7 +424,7 @@
             dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.DgPatientsUpdate.DefaultCellStyle = dataGridViewCellStyle8;
-            this.DgPatientsUpdate.Location = new System.Drawing.Point(115, 149);
+            this.DgPatientsUpdate.Location = new System.Drawing.Point(107, 155);
             this.DgPatientsUpdate.Name = "DgPatientsUpdate";
             this.DgPatientsUpdate.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
@@ -438,7 +438,7 @@
             dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle10.Font = new System.Drawing.Font("David", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.DgPatientsUpdate.RowsDefaultCellStyle = dataGridViewCellStyle10;
-            this.DgPatientsUpdate.Size = new System.Drawing.Size(722, 258);
+            this.DgPatientsUpdate.Size = new System.Drawing.Size(548, 258);
             this.DgPatientsUpdate.TabIndex = 65;
             // 
             // clinic
@@ -451,7 +451,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(950, 584);
+            this.ClientSize = new System.Drawing.Size(685, 475);
             this.Controls.Add(this.TabGroups);
             this.Name = "F_Groups";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
